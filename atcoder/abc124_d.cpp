302b712a2de8981c0ca31b8e3d5566676557ad09
gifmr <ntkbusy0213@gmail.com>
2019-11-29 22:04:42 +0900

    add

---
+++ atcoder/abc124_d.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = (int)(0); i < (int)(n); ++i)
+#define reps(i, n) for (int i = (int)(1); i <= (int)(n); ++i)
+#define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rreps(i, n) for (int i = ((int)(n)); i > 0; i--)
+#define irep(i, m, n) for (int i = (int)(m); i < (int)(n); ++i)
+#define ireps(i, m, n) for (int i = (int)(m); i <= (int)(n); ++i)
+#define FOR(e, c) for (auto &e : c)
+#define SORT(v, n) sort(v, v + n);
+#define vsort(v) sort(v.begin(), v.end());
+#define rvisort(v) sort(v.begin(), v.end(), greater<int>());
+#define all(v) v.begin(), v.end()
+#define mp(n, m) make_pair(n, m);
+#define cout(d) cout<<d<<endl;
+#define coutd(d) cout<<std::setprecision(10)<<d<<endl;
+#define cinline(n) getline(cin,n);
+#define replace_all(s, b, a) replace(s.begin(),s.end(), b, a);
+// #define int long long
+#define PI (acos(-1))
+#define FILL(v, n, x) fill(v, v + n, x);
+#define sz(x) int(x.size())
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pii = pair<int, int>;
+using ul = unsigned long;
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int GCD(int a, int b) { return b ? GCD(b, a%b) : a; }
+int LCM(int a, int b) { return a * b / GCD(a, b); }
+
+const int dy[] = {0, 1, 0, -1, -1, 1, 1, -1};
+const int dx[] = {1, 0, -1, 0, 1, 1, -1, -1};
+
+inline bool inside(int y, int x, int H, int W) {
+    return (y >= 0 && x >= 0 && y < H && x < W);
+}
+
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+signed main()
+{
+  int n,k; cin>>n>>k;
+  string s; cin>>s;
+  
+  vi nums;
+  int now=1;
+  int cnt=0;
+  for(int i=0;i<n;i++){
+    if(s[i] == '0'+now)cnt++;
+    else{
+      nums.push_back(cnt);
+      now=1-now;
+      cnt=1;
+    }
+  }
+  nums.push_back(cnt);
+  if(s[n-1]=='0')nums.push_back(0);
+  int m=nums.size();
+  vi sums(m+1);
+  for(int i=0;i<m;i++){
+    sums[i+1]=sums[i]+nums[i];
+  }
+  int ans=0;
+  int point=2*k+1;
+  for(int i=0;i<m+1;i+=2){
+    int tmp=sums[min(point+i,m)]-sums[i];
+    chmax(ans,tmp);
+  }
+  cout<<ans<<endl;
+}
