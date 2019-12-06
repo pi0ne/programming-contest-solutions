9cb24a9bb836345b46954519ec4d1985d5213023
gifmr <ntkbusy0213@gmail.com>
2019-12-07 01:27:02 +0900

    add

---
+++ atcoder/abc097_c.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = (int)(0); i < (int)(n); ++i)
+#define irep(i, m, n) for (int i = (int)(m); i < (int)(n); ++i)
+#define ireps(i, m, n) for (int i = (int)(m); i <= (int)(n); ++i)
+#define FOR(e, c) for (auto &e : c)
+#define SORT(v, n) sort(v, v + n);
+#define vsort(v) sort(v.begin(), v.end());
+#define rvisort(v) sort(v.begin(), v.end(), greater<int>());
+#define all(v) v.begin(), v.end()
+#define mp(n, m) make_pair(n, m);
+#define cout(d) cout<<d<<endl;
+#define coutd(d) cout<<std::setprecision(10)<<d<<endl;
+#define cinline(n) getline(cin,n);
+#define replace_all(s, b, a) replace(s.begin(),s.end(), b, a);
+// #define int long long
+#define sz(x) int(x.size())
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pii = pair<int, int>;
+using ul = unsigned long;
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int GCD(int a, int b) { return b ? GCD(b, a%b) : a; }
+int LCM(int a, int b) { return a * b / GCD(a, b); }
+
+inline bool inside(int y, int x, int H, int W) {
+    return (y >= 0 && x >= 0 && y < H && x < W);
+}
+
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+
+signed main()
+{
+  string s; cin>>s;
+  int k; cin>>k;
+  
+  vector<string> a;
+  rep(i,s.size()){
+    for(int j=i;j<min((int)s.size(),i+k);j++){
+      a.push_back(s.substr(i,(j-i+1)));
+    }
+  }
+  vsort(a);
+  string pre=a[0];
+  if(k==1){
+    cout<<pre<<endl;
+    return 0;
+  }
+  string ans;
+  int cnt=1;
+  int i=0;
+  for(auto e: a){
+    i++;
+    if(pre==a[i])continue;
+    pre=a[i];
+    cnt++;
+    if(cnt==k) {
+      ans=a[i];
+      break;
+    }
+  }
+  cout<<ans<<endl;
+}
