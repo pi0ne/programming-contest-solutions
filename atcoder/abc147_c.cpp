60deac7d8f759f43a4f334d18baf25ef3efd6cd9
gifmr <ntkbusy0213@gmail.com>
2019-12-09 00:33:47 +0900

    add comment

--- atcoder/abc147_c.cpp
+++ atcoder/abc147_c.cpp
@@ -1,47 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, n) for (int i = (int)(0); i < (int)(n); ++i)
-#define reps(i, n) for (int i = (int)(1); i <= (int)(n); ++i)
-#define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
-#define rreps(i, n) for (int i = ((int)(n)); i > 0; i--)
-#define irep(i, m, n) for (int i = (int)(m); i < (int)(n); ++i)
-#define ireps(i, m, n) for (int i = (int)(m); i <= (int)(n); ++i)
-#define FOR(e, c) for (auto &e : c)
-#define SORT(v, n) sort(v, v + n);
-#define vsort(v) sort(v.begin(), v.end());
-#define rvisort(v) sort(v.begin(), v.end(), greater<int>());
-#define all(v) v.begin(), v.end()
-#define mp(n, m) make_pair(n, m);
-#define cout(d) cout<<d<<endl;
-#define coutd(d) cout<<std::setprecision(10)<<d<<endl;
-#define cinline(n) getline(cin,n);
-#define replace_all(s, b, a) replace(s.begin(),s.end(), b, a);
-// #define int long long
-#define PI (acos(-1))
-#define FILL(v, n, x) fill(v, v + n, x);
-#define sz(x) int(x.size())
-
-using ll = long long;
-using vi = vector<int>;
-using vvi = vector<vi>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
-using pii = pair<int, int>;
-using ul = unsigned long;
-
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
-template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
-
-int GCD(int a, int b) { return b ? GCD(b, a%b) : a; }
-int LCM(int a, int b) { return a * b / GCD(a, b); }
-
-const int dy[] = {0, 1, 0, -1, -1, 1, 1, -1};
-const int dx[] = {1, 0, -1, 0, 1, 1, -1, -1};
-
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
 
 int a[20];
 int x[20][20];
@@ -64,12 +24,26 @@ signed main()
     }
   }
   int ans=0;
+  // bits ビットが立っているところが人iが正しい証言をしていることを意味する
   for(int bits=1; bits<(1<<n); bits++){
     bool ok=true;
+    // 最大15人の人iについてのループ
     for(int i=1; i<=n; i++){
+      // ビットiが立っている時だけ処理する
+      // 正しい証言をしていると仮定しているもの(=ビットが立っている)だけ確認すればよい
+      // iが立っていないものは「不確か」であって、「不正確であること」では無い事に注意
       if(!(bits & (1<<(i-1)))) continue;
+      // 各人iの証言についてのループ
       for(int j=1; j<=a[i]; j++){
-        if(((bits>>x[i][j]-1)&1) ^ y[i][j]) ok = false;
+        // 矛盾しないかを判定する
+        // 0^1 (bits上は不親切と仮定したのに正直者であると証言してしまっている)
+        // 1^0 (正直者と仮定したのに不親切と証言してしまっている)
+        if(((bits>>x[i][j]-1)&1) ^ y[i][j]) {
+          // bits上正しいと仮定した人iの証言が全て矛盾しなければここを通らず、
+          // bitsの立っているビットの数が、正直者の数と同値になる
+          ok = false;
+          break;
+        }
       }
     }
     if(ok) chmax(ans, counter(bits));
