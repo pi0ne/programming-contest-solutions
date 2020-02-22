a06f60b1a2c031b603276116aaee498a79e7ff88
gifmr <ntkbusy0213@gmail.com>
2020-02-23 03:00:54 +0900

    add

---
+++ atcoder/edpc_l2.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = (int)(0); i < (int)(n); ++i)
+#define reps(i, n) for (int i = (int)(1); i <= (int)(n); ++i)
+#define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rreps(i, n) for (int i = ((int)(n)); i > 0; i--)
+#define irep(i, m, n) for (int i = (int)(m); i < (int)(n); ++i)
+#define ireps(i, m, n) for (int i = (int)(m); i <= (int)(n); ++i)
+#define FOR(e, c) for (auto &e : c)
+#define SORT(v, n) sort(v, v + n);
+#define vsort(v) sort(v.begin(), v.end());
+#define rvisort(v) sort(v.begin(), v.end(), greater<int>());
+#define all(v) v.begin(), v.end()
+#define mp(n, m) make_pair(n, m);
+#define cout(d) cout<<d<<endl;
+#define coutd(d) cout<<std::setprecision(10)<<d<<endl;
+#define cinline(n) getline(cin,n);
+#define replace_all(s, b, a) replace(s.begin(),s.end(), b, a);
+// #define int long long
+#define PI (acos(-1))
+#define FILL(v, n, x) fill(v, v + n, x);
+#define sz(x) int(x.size())
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pii = pair<int, int>;
+using ul = unsigned long;
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e15;
+
+ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
+ll LCM(ll a, ll b) { return a * b / GCD(a, b); }
+
+const int dy[] = {0, 1, 0, -1, -1, 1, 1, -1};
+const int dx[] = {1, 0, -1, 0, 1, 1, -1, -1};
+
+inline bool inside(int y, int x, int H, int W) {
+    return (y >= 0 && x >= 0 && y < H && x < W);
+}
+
+ll dp[3005][3005];
+
+int main(){
+  int n; cin>>n;
+  vi a(n);
+  rep(i,n) cin>>a[i];
+  
+  rep(i,n) dp[i][i]=0;
+  
+  for(int len=1; len<=n; ++len){
+    for(int i=0; i+len<=n; i++){
+      int j=i+len; // 半開区間[i,j)の右端
+      if((n-len)%2==0) dp[i][j]=max(dp[i+1][j]+a[i], dp[i][j-1]+a[j-1]);
+      else dp[i][j]=min(dp[i+1][j]-a[i], dp[i][j-1]-a[j-1]);
+    }
+  }
+  cout<<dp[0][n]<<endl;
+}
