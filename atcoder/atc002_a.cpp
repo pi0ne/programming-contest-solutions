dae422d6df54169786e354b596d13afb9bd1de61
gifmr <ntkbusy0213@gmail.com>
2020-02-29 07:03:09 +0900

    update

--- atcoder/atc002_a.cpp
+++ atcoder/atc002_a.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define rep(i, n) for (int i = (int)(0); i < (int)(n); ++i)
 #define reps(i, n) for (int i = (int)(1); i <= (int)(n); ++i)
 #define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
@@ -21,7 +21,7 @@ using namespace std;
 #define PI (acos(-1))
 #define FILL(v, n, x) fill(v, v + n, x);
 #define sz(x) int(x.size())
-
+ 
 using ll = long long;
 using vi = vector<int>;
 using vvi = vector<vi>;
@@ -29,52 +29,50 @@ using vll = vector<ll>;
 using vvll = vector<vll>;
 using pii = pair<int, int>;
 using ul = unsigned long;
-
+ 
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
-
+ 
 ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
 ll LCM(ll a, ll b) { return a / GCD(a, b)* b; }
-
+ 
 const int dy[] = {0, 1, 0, -1, -1, 1, 1, -1};
 const int dx[] = {1, 0, -1, 0, 1, 1, -1, -1};
-
+ 
 inline bool inside(int y, int x, int H, int W) {
     return (y >= 0 && x >= 0 && y < H && x < W);
 }
-
+ 
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
 
-// https://atcoder.jp/contests/atc002/tasks/abc007_3
+int r,c,sy,sx,gy,gx; 
+char G[55][55];
+int cost[55][55];
 
 signed main()
 {
   cin.tie( 0 ); ios::sync_with_stdio( false );
-  int h,w; cin>>h>>w;
-  int sy,sx,gy,gx; cin>>sy>>sx>>gy>>gx;
-  sy--;sx--;gy--;gx--;
-  char c[h][w];
-  rep(i,h) rep(j,w) cin>>c[i][j];
-  int dist[h][w];
-  rep(i,h) rep(j,w) dist[i][j]=-1;
-  dist[sy][sy]=0;
+  
+  cin>>r>>c>>sy>>sx>>gy>>gx;
+  sy--,sx--,gy--,gx--;
+  rep(i,r) rep(j,c) cin>>G[i][j];
+  
   queue<pii> q;
   q.emplace(sy,sx);
   while(!q.empty()){
-    int y=q.front().first;
-    int x=q.front().second;
+    int y=q.front().first, x=q.front().second;
     q.pop();
     rep(i,4){
-      int ny=y+dy[i];
-      int nx=x+dx[i];
-      if(c[ny][nx]=='#') continue;
-      if(dist[ny][nx]!=-1) continue;
-      if(!inside(ny,nx,h,w)) continue;
-      dist[ny][nx]=dist[y][x]+1;
-      q.emplace(ny,nx);
+      int ny=y+dy[i], nx=x+dx[i];
+      if(inside(ny,nx,r,c)&&G[ny][nx]!='#'){
+        if(cost[ny][nx]!=0) continue;
+        cost[ny][nx]=cost[y][x]+1;
+        q.emplace(ny,nx);
+      }
     }
   }
-  cout<<dist[gy][gx]<<endl;
+  
+  cout<<cost[gy][gx]<<endl;
 }
