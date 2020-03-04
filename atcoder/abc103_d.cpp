6b304fde8ca8b85ec89cda51b48db749ab761a10
gifmr <ntkbusy0213@gmail.com>
2020-03-04 18:26:21 +0900

    add

---
+++ atcoder/abc103_d.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = (int)(0); i < (int)(n); ++i)
+#define reps(i, n) for (int i = (int)(1); i <= (int)(n); ++i)
+#define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rreps(i, n) for (int i = ((int)(n)); i > 0; i--)
+#define irep(i, m, n) for (int i = (int)(m); i < (int)(n); ++i)
+#define ireps(i, m, n) for (int i = (int)(m); i <= (int)(n); ++i)
+#define FOR(e, c) for (auto &e : c)
+#define SORT(v, n) sort(v, v + n);
+#define vsort(v) sort(v.begin(), v.end());
+#define rvisort(v) sort(v.begin(), v.end(), greater<int>());
+#define all(v) v.begin(), v.end()
+#define mp(n, m) make_pair(n, m);
+#define cout(d) cout<<d<<endl;
+#define coutd(d) cout<<std::setprecision(10)<<d<<endl;
+#define cinline(n) getline(cin,n);
+#define replace_all(s, b, a) replace(s.begin(),s.end(), b, a);
+#define int long long
+#define PI (acos(-1))
+#define FILL(v, n, x) fill(v, v + n, x);
+#define sz(x) int(x.size())
+#define pqasc priority_queue<long long, vector<long long>, greater<long long>>
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using ul = unsigned long;
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int GCD(int a, int b) { return b ? GCD(b, a%b) : a; }
+int LCM(int a, int b) { return a * b / GCD(a, b); }
+
+const int dy[] = {0, 1, 0, -1, -1, 1, 1, -1};
+const int dx[] = {1, 0, -1, 0, 1, 1, -1, -1};
+
+inline bool inside(int y, int x, int H, int W) {
+    return (y >= 0 && x >= 0 && y < H && x < W);
+}
+
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+signed main()
+{
+  cin.tie( 0 ); ios::sync_with_stdio( false );
+  int n,m; cin>>n>>m;
+  vector<pll> v(m);
+  rep(i,m){
+    int a,b; cin>>a>>b;
+    a--, b--;
+    v[i]={b,a};
+  }
+  sort(v.begin(),v.end());
+  
+  int ans=0;
+  int now=-1;
+  rep(i,m){
+    if(now<=v[i].second){
+      ans++;
+      now=v[i].first;
+    }
+  }
+  cout<<ans<<endl;
+}
