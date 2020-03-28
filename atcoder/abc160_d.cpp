e4926596c6d77a6d6f163c2e076d9cd7b3b22bc8
gifmr <ntkbusy0213@gmail.com>
2020-03-29 01:25:07 +0900

    add

---
+++ atcoder/abc160_d.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define int long long
+#define rep(i, n) for (int i = (int)(0); i < (int)(n); ++i)
+#define reps(i, n) for (int i = (int)(1); i <= (int)(n); ++i)
+#define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rreps(i, n) for (int i = ((int)(n)); i > 0; i--)
+#define irep(i, m, n) for (int i = (int)(m); i < (int)(n); ++i)
+#define ireps(i, m, n) for (int i = (int)(m); i <= (int)(n); ++i)
+#define SORT(v, n) sort(v, v + n);
+#define REVERSE(v, n) reverse(v, v+n);
+#define vsort(v) sort(v.begin(), v.end());
+#define all(v) v.begin(), v.end()
+#define mp(n, m) make_pair(n, m);
+#define cout(d) cout<<d<<endl;
+#define coutd(d) cout<<std::setprecision(10)<<d<<endl;
+#define cinline(n) getline(cin,n);
+#define replace_all(s, b, a) replace(s.begin(),s.end(), b, a);
+#define PI (acos(-1))
+#define FILL(v, n, x) fill(v, v + n, x);
+#define sz(x) int(x.size())
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vs = vector<string>;
+using vpll = vector<pair<ll, ll>>;
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+const ll INF = 1e15;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+ll dist[2005][2005];
+
+signed main()
+{
+  cin.tie( 0 ); ios::sync_with_stdio( false );
+  ll n,x,y; cin>>n>>x>>y;
+  x--,y--;
+  
+  rep(i,n){
+    irep(j,i+1,n){
+      dist[i][j]=dist[j][i]=abs(i-j);
+    }
+  }
+  
+  rep(i,n) rep(j,n) dist[i][j]=min(dist[i][j],dist[i][x]+dist[y][j]+1);
+  
+  vll ans(n);
+  rep(i,n) irep(j,i+1,n) ans[dist[i][j]]++;
+  reps(i,n-1) cout<<ans[i]<<endl;
+}
