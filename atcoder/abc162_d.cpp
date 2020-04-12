40f8699f9af2c23a3db934eb5cfebf716f67cf8d
gifmr <ntkbusy0213@gmail.com>
2020-04-12 23:01:58 +0900

    add

---
+++ atcoder/abc162_d.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define int long long
+#define rep(i, n) for (int i = (int)(0); i < (int)(n); ++i)
+#define reps(i, n) for (int i = (int)(1); i <= (int)(n); ++i)
+#define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rreps(i, n) for (int i = ((int)(n)); i > 0; i--)
+#define irep(i, m, n) for (int i = (int)(m); i < (int)(n); ++i)
+#define ireps(i, m, n) for (int i = (int)(m); i <= (int)(n); ++i)
+#define SORT(v, n) sort(v, v + n);
+#define REVERSE(v, n) reverse(v, v+n);
+#define vsort(v) sort(v.begin(), v.end());
+#define all(v) v.begin(), v.end()
+#define mp(n, m) make_pair(n, m);
+#define cout(d) cout<<d<<endl;
+#define coutd(d) cout<<std::setprecision(10)<<d<<endl;
+#define cinline(n) getline(cin,n);
+#define replace_all(s, b, a) replace(s.begin(),s.end(), b, a);
+#define PI (acos(-1))
+#define FILL(v, n, x) fill(v, v + n, x);
+#define sz(x) int(x.size())
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vs = vector<string>;
+using vpll = vector<pair<ll, ll>>;
+using vtp = vector<tuple<ll,ll,ll>>;
+using vb = vector<bool>;
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+const ll INF = 1e15;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+
+
+signed main()
+{
+  cin.tie( 0 ); ios::sync_with_stdio( false );
+  ll n; string s;
+  cin>>n>>s;
+  ll ans=0;
+  ll r=0,g=0,b=0;
+  rep(i,n){
+    if(s[i]=='R') r++;
+    else if(s[i]=='G') g++;
+    else b++;
+  }
+  ans=r*g*b;
+  ll tmp=0;
+  ll span=1;
+  while(span<=n){
+    for(ll i=0; i+2*span<n; i++){
+      if(s[i]!=s[i+span] && s[i]!=s[i+2*span] && s[i+span]!=s[i+2*span]) tmp++;
+    }
+    span++;
+  }
+  cout<<ans-tmp<<endl;
+}
